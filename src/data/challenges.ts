d241cfb9bf6e181ac7d5240d57d5ce81835b1d8c
Aleksefo <aleksefo@gmail.com>
2017-02-19 06:57:03 +0200

    Challenge component and challenge data provider added

---
+++ src/data/challenges.ts
@@ -0,0 +1,17 @@
+export default [
+            {
+                id: '1',
+                title: 'Title1',
+                descr: 'Description1'
+            },
+            {
+                id: '2',
+                title: 'Title2',
+                descr: 'Description2'
+            },
+            {
+                id: '3',
+                title: 'Title3',
+                descr: 'Description3'
+            }
+]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import {Challenge} from "../../data/challenge.interface";
+import challenges from "../../data/challenges";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  chCollection: {}[];
+
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ngOnInit() {
+    this.chCollection = challenges;
   }
 
 }
