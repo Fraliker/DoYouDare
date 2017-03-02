6900e8248099564003aba3efe59f0eba2687cec1
Aleksefo <aleksefo@gmail.com>
2017-03-02 09:42:18 +0200

    Side menu navigation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {Platform, NavController, MenuController} from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import {FrontPage} from "../pages/front/front";
+import {TabsPage} from "../pages/tabs/tabs";
+import {LoginPage} from "../pages/login/login";
+import {SignupPage} from "../pages/signup/signup";
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = FrontPage;
+//  rootPage = FrontPage;
+  tabsPage = TabsPage;
+  loginPage = LoginPage;
+  signupPage = SignupPage;
+  @ViewChild('nav') nav: NavController;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, private menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  onLoad(page: any) {
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
+  }
+
+  onLogout() {
+
   }
 }
