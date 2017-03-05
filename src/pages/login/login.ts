4c2f71e7698f127bad7678f714a51e5e3a3de8f3
Aleksefo <aleksefo@gmail.com>
2017-03-05 23:41:00 +0200

    Signin functionality, error msg and animation

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NgForm} from "@angular/forms";
+import {AuthService} from "../../services/auth";
+import {LoadingController, AlertController} from "ionic-angular";
 
 
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
+    selector: 'page-login',
+    templateUrl: 'login.html'
 })
 export class LoginPage {
 
-onLogin(form: NgForm) {
-  console.log(form.value);
-}
+    constructor(private authService: AuthService,
+    private loadingCtrl: LoadingController,
+                private alertCtrl: AlertController
+) {
+    }
+
+    onLogin(form: NgForm) {
+        const loading = this.loadingCtrl.create({
+            content: 'Signing you in...'
+        });
+        loading.present();
+        this.authService.signin(form.value.email, form.value.password)
+            .then(data => {
+                loading.dismiss();
+            })
+            .catch(error => {
+                loading.dismiss();
+                const alert = this.alertCtrl.create({
+                    title: 'Signing failed!',
+                    message: error.message,
+                    buttons: ['Ok']
+                });
+                alert.present();
+            })
+    }
 
 }
