a68194c85d1911ace82c4561a46133465669b6c6
Aleksefo <aleksefo@gmail.com>
2017-03-05 23:24:15 +0200

    Signup error msg and animation

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {AuthService} from "../../services/auth";
+import {LoadingController, AlertController} from "ionic-angular";
 
 
 @Component({
-  selector: 'page-signup',
-  templateUrl: 'signup.html'
+    selector: 'page-signup',
+    templateUrl: 'signup.html'
 })
 export class SignupPage {
-  constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService,
+                private loadingCtrl: LoadingController,
+                private alertCtrl: AlertController
+    ) {
+    }
 
-  onSignup(form: NgForm) {
-    this.authService.signup(form.value.email, form.value.password)
-        .then( data => console.log(data))
-        .catch( error => console.log(error));
-  }
+    onSignup(form: NgForm) {
+        const loading = this.loadingCtrl.create({
+            content: 'Signing you up...'
+        });
+        this.authService.signup(form.value.email, form.value.password)
+            .then(data => {
+                loading.dismiss();
+                })
+            .catch(error => {
+                loading.dismiss();
+                const alert = this.alertCtrl.create({
+                    title: 'Signup failed!',
+                    message: error.message,
+                    buttons: ['Ok']
+                });
+                alert.present();
+            })
+    }
 }
